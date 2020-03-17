08a6abbd4e2e907c5556e47f398f9ed8705d466a
Alexander Morozov <Alexander.Morozov@itiviti.com>
2020-03-17 17:40:48 +0100

    Add more tests

--- src/test_randomized_queue.cpp
+++ src/test_randomized_queue.cpp
@@ -21,6 +21,12 @@ public:
         return *this;
     }
 
+    NonCopyable & operator *= (const NonCopyable & other)
+    {
+        m_data *= other.m_data;
+        return *this;
+    }
+
     friend bool operator == (const int lhs, const NonCopyable & rhs)
     { return lhs == rhs.m_data; }
     friend bool operator == (const NonCopyable & lhs, const int rhs)
@@ -89,21 +95,22 @@ TYPED_TEST(RandomizedQueueTest, many)
     const auto b2 = this->queue.cbegin();
     const auto e2 = this->queue.cend();
 
-    std::vector<int> v11, v12;
+    std::vector<int> v11, v12, v21, v22;
     std::copy(b1, e1, std::back_inserter(v11));
+    std::copy(b2, e2, std::back_inserter(v21));
     std::copy(b1, e1, std::back_inserter(v12));
+    std::copy(b2, e2, std::back_inserter(v22));
     EXPECT_EQ(etalon_sorted.size(), v11.size());
     EXPECT_EQ(v11, v12);
     std::sort(v12.begin(), v12.end());
     EXPECT_EQ(etalon_sorted, v12);
-
-    std::vector<int> v21, v22;
-    std::copy(b2, e2, std::back_inserter(v21));
-    std::copy(b2, e2, std::back_inserter(v22));
     EXPECT_EQ(etalon_sorted.size(), v21.size());
     EXPECT_EQ(v21, v22);
-
     EXPECT_NE(v11, v21);
+
+    for (int i = 0; i < 100; ++i) {
+        EXPECT_NE(etalon_sorted.end(), std::find(etalon_sorted.begin(), etalon_sorted.end(), this->queue.sample()));
+    }
 
     std::vector<int> v;
     v.reserve(this->queue.size());
@@ -113,4 +120,84 @@ TYPED_TEST(RandomizedQueueTest, many)
     EXPECT_EQ(etalon_sorted.size(), v.size());
     std::sort(v.begin(), v.end());
     EXPECT_EQ(etalon_sorted, v);
+}
+
+TYPED_TEST(RandomizedQueueTest, iterator_modify)
+{
+    const std::vector<int> initial_values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    const std::vector<int> etalon_sorted = {0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100};
+    for (auto i : initial_values) {
+        this->queue.enqueue(i);
+    }
+    EXPECT_FALSE(this->queue.empty());
+    EXPECT_EQ(initial_values.size(), this->queue.size());
+    for (auto & x : this->queue) {
+        x *= x;
+    }
+    std::vector<int> values;
+    values.reserve(this->queue.size());
+    for (const auto & x : this->queue) {
+        values.push_back(x);
+    }
+    EXPECT_EQ(initial_values.size(), values.size());
+    std::sort(values.begin(), values.end());
+    EXPECT_EQ(etalon_sorted, values);
+}
+
+TYPED_TEST(RandomizedQueueTest, lot_of_modifications)
+{
+    int first = 1234;
+    int second = 1234 + 150000;
+    int third = second + 150000;
+    int fourth = third + 150000;
+    const std::size_t n1 = second - first;
+    const std::size_t n2 = third - first;
+    const std::size_t n3 = fourth - first;
+    for (int i = first; i < second; ++i) {
+        this->queue.enqueue(i);
+    }
+    EXPECT_EQ(n1, this->queue.size());
+    for (int i = second; i < third; ++i) {
+        this->queue.enqueue(i);
+    }
+    EXPECT_EQ(n2, this->queue.size());
+
+    {
+        std::size_t count = 0;
+        for (const auto & x : this->queue) {
+            ++count;
+            EXPECT_LE(first, x);
+            EXPECT_GE(third, x);
+        }
+        EXPECT_EQ(n2, count);
+    }
+
+    for (std::size_t i = 0; i < n1; ++i) {
+        const int x = this->queue.dequeue();
+        EXPECT_LE(first, x);
+        EXPECT_GE(third, x);
+    }
+    EXPECT_EQ(n2 - n1, this->queue.size());
+    for (int i = third; i < fourth; ++i) {
+        this->queue.enqueue(i);
+    }
+
+    {
+        std::size_t count = 0;
+        for (const auto & x : this->queue) {
+            ++count;
+            EXPECT_LE(first, x);
+            EXPECT_GE(fourth, x);
+        }
+        EXPECT_EQ(n3 - n1, count);
+    }
+
+    std::size_t count = 0;
+    while (!this->queue.empty()) {
+        ++count;
+        const int x = this->queue.dequeue();
+        EXPECT_LE(first, x);
+        EXPECT_GE(fourth, x);
+    }
+    EXPECT_EQ(n3 - n1, count);
 }
